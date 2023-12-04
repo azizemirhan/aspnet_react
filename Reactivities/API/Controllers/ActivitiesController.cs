8ef69cfcb821aa89cce487e335c6a9124791af7f
azizemirhan <azizemirhanozdemir@gmail.com>
2023-12-04 15:12:58 +0300

    first commit

---
+++ Reactivities/API/Controllers/ActivitiesController.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+        public readonly DataContext _contex;
+        public ActivitiesController(DataContext contex)
+        {
+            _contex = contex;
+            
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities(){
+            return await _contex.Activities.ToListAsync();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivity(Guid id){
+            return await _contex.Activities.FindAsync(id);
+        }
+    }
+}
