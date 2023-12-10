901597c0cc3748376fe651093e640db7f806f3dd
azizemirhan <azizemirhanozdemir@gmail.com>
2023-12-10 07:55:02 +0300

    six commit

--- Reactivities/client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ Reactivities/client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -14,14 +14,15 @@ interface Props{
     openForm: (id:string) => void;
     closeForm: () => void;
     createOrEdit: (activity: Activity) => void;
-    deleteActivity: (id:String) => void;
+    deleteActivity: (id:string) => void;
+    submitting: boolean;
 }
 
-export default function ActivityDashboard({activities, selectedActivity, selectActivity, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit, deleteActivity}:Props){
+export default function ActivityDashboard({activities, selectedActivity, selectActivity, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit, deleteActivity, submitting}:Props){
     return (
         <Grid>
             <Grid.Column width='10'>
-                <ActivityList activites={activities} selectActivity={selectActivity} deleteActivity={deleteActivity}></ActivityList>
+                <ActivityList activites={activities} selectActivity={selectActivity} deleteActivity={deleteActivity} submitting={submitting}></ActivityList>
             </Grid.Column>
             <Grid.Column width='6'>
                 {
@@ -35,7 +36,10 @@ export default function ActivityDashboard({activities, selectedActivity, selectA
                 {
                     editMode && 
                 <ActivityForm  
-                        closeForm={closeForm} activity={selectedActivity}  createOrEdit={createOrEdit} 
+                        closeForm={closeForm} 
+                        activity={selectedActivity}  
+                        createOrEdit={createOrEdit} 
+                        submitting={submitting}
                     />
                 }
             </Grid.Column>
--- Reactivities/client-app/src/features/activities/dashboard/ActivityList.tsx
+++ Reactivities/client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -6,9 +6,10 @@ interface Props {
     activites: Activity[];
     selectActivity: (id: string) => void;
     deleteActivity: (id: string) => void;
+    submitting: boolean;
 }
 
-export default function ActivityList({activites, selectActivity, deleteActivity} : Props){
+export default function ActivityList({activites, selectActivity, deleteActivity, submitting} : Props){
     return (
         <Segment>
             <Item.Group divided>
@@ -23,7 +24,7 @@ export default function ActivityList({activites, selectActivity, deleteActivity}
                                 </Item.Description>
                                 <Item.Extra>
                                     <Button onClick={() => selectActivity(activity.id)} floated="right" content="View" color="blue"/>
-                                    <Button onClick={() => deleteActivity(activity.id)} floated="right" content="Delete" color="red"/>
+                                    <Button loading={submitting} onClick={() => deleteActivity(activity.id)} floated="right" content="Delete" color="red"/>
                                     <Label basic content={activity.category} />
                                 </Item.Extra>
                         </ItemContent>
